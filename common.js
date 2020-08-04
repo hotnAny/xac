77f7dfdd24c4c0aebb9d9095dccdb5162a07b1b5
Xiang 'Anthony' Chen <xac@xiangs-mbp.lan>
2020-08-03 17:27:57 -0700

    added test.js

--- common.js
+++ common.js
@@ -1,14 +1,14 @@
 let XAC = XAC || {}
 
-let log = () => {
+XAC.log = () => {
     console.log(arguments.join(' '))
 }
 
-let tic = (msg) => {
+XAC.tic = (msg) => {
     if (XAC.t0 != undefined) {
         let t1 = new Date().getMilliseconds()
         msg = msg || 'time elapsed'
-        log(msg, ':', t1 - XAC.t0)
+        XAC.log(msg, ':', t1 - XAC.t0)
     }
     XAC.t0 = new Date().getMilliseconds()
 }
